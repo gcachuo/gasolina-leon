5652705450b186aafdc7137c5239b51d0f16e9dd
Memo Cachú <gcachu.o@gmail.com>
2019-01-08 17:52:29 -0600

    Finish webpack

---
+++ webpack/src/index.js
@@ -0,0 +1,4 @@
+$ = JQuery = require('jquery');
+Mustache = require('mustache');
+Project = require('./js/project.js');
+require('./scss/main.scss');
---
+++ webpack/src/js/project.js
@@ -0,0 +1,94 @@
+const host = 'https://gasolina-leon-api.herokuapp.com/';
+const api = '/';
+const init = function () {
+    Project.navigate(Project.getCookie('page') || 'dashboard');
+};
+const load = function () {
+
+};
+
+const Project = {
+    init: init,
+    load: function () {
+        Project.host = localStorage.getItem('host') || host;
+        Project.url = Project.host + api;
+        $("form").on('submit', event => {
+            const $this = $(event.currentTarget);
+            event.preventDefault();
+            Project.request($this.data('action'), $this.serializeArray(), 'POST').done(data => {
+                if (typeof data.response === 'string') {
+                    alert(data.response);
+                }
+                Project.navigate($this.data('redirect'), data);
+            });
+        });
+        load();
+    },
+    navigate: function (file, data) {
+        return $.get('pages/' + file + ".html", function (template) {
+            const rendered = Mustache.render(template, data || {});
+            $(".app").html(rendered);
+            Project.setCookie('page', file, 1);
+            Project.load();
+        }, 'html');
+    },
+    request: function (uri, data, method) {
+        if (!uri) return;
+        return $.ajax(Project.url + uri, {
+            method: method || 'GET',
+            dataType: 'json',
+            data: data,
+            error: response => {
+                let message;
+                if (response.responseJSON) {
+                    const result = response.responseJSON;
+                    switch (result.code) {
+                        case 500:
+                            console.error(result.error.message);
+                            break;
+                        case 400:
+                            alert(result.error.message);
+                            return;
+                        default:
+                            console.error(result);
+                            break;
+                    }
+                } else if (response.responseText) {
+                    console.error(`${Project.url} ${response.responseText}`);
+                }
+                if (response.statusText === "error") {
+                    console.log(response);
+                    message = "Can't reach server.";
+
+                } else {
+                    message = 'An error ocurred.';
+                }
+                Project.toast({message: message, type: 'error', duration: 2000});
+            }
+        });
+    },
+    setCookie: function (name, value, days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    },
+    getCookie: function (name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    },
+    toast: function (options) {
+        if (!options.type) options.type = "info";
+        toastr[options.type](options.message, options.title, {timeOut: options.duration})
+    }
+};
+module.exports = Project;
