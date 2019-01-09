d0fe9d6345d3599a1f39729e326e896bac698c81
Memo Cachú <gcachu.o@gmail.com>
2019-01-09 01:33:20 -0600

    zoom

--- webpack/src/js/_maps.js
+++ webpack/src/js/_maps.js
@@ -1,14 +1,32 @@
+const
+    api_key = {
+        release: 'AIzaSyBFMFeQ5dKQYaILCAY0kxoACcftBHlsjfI',
+        debug: 'AIzaSyBQg1lHyydFoB2Sxwud1GgIQTFnXzaO_wg'
+    },
+    target = {
+        lat: 21.1228352,
+        lng: -101.6755628
+    },
+    zoom = 13;
+
 Project.Maps = {
     init: function () {
         plugin.google.maps.environment.setEnv({
-            'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyBFMFeQ5dKQYaILCAY0kxoACcftBHlsjfI',
-            'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyBQg1lHyydFoB2Sxwud1GgIQTFnXzaO_wg'
+            'API_KEY_FOR_BROWSER_RELEASE': api_key.release,
+            'API_KEY_FOR_BROWSER_DEBUG': api_key.debug
         });
         // Define a div tag with id="map_canvas"
         var mapDiv = $("#map_canvas").get()[0];
 
+        var options = {
+            camera: {
+                target: target,
+                zoom: zoom
+            }
+        };
+
         // Initialize the map plugin
-        var map = plugin.google.maps.Map.getMap(mapDiv);
+        var map = plugin.google.maps.Map.getMap(mapDiv, options);
 
         // The MAP_READY event notifies the native map view is fully ready to use.
         map.one(plugin.google.maps.event.MAP_READY, this.onMapInit);
